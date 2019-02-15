c14905e492161c9c7a9f6b99932fae91c4a5d629
Yuichi Motoyama <yomichi@tsg.jp>
2019-02-15 13:30:11 +0900

    rm scripts

--- tool/dla_latgen.py
+++
@@ -1,4 +0,0 @@
-#!python
-from dsqss.std_lattice import main
-if __name__ == '__main__':
-    main()
--- tool/hamgen.py
+++
@@ -1,4 +0,0 @@
-#!python
-from dsqss.std_model import main
-if __name__ == '__main__':
-    main()
