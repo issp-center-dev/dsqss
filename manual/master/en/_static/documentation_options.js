179838a94f92b6c5f61fc25a15084106f123522c
DSQSS <dsqss-dev@issp.u-tokyo.ac.jp>
2019-05-23 01:17:10 +0000

    Update by TravisCI (536057612)

--- manual/master/en/_static/documentation_options.js
+++ manual/master/en/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.0-DEV',
+    VERSION: '2.0.0-Beta',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
