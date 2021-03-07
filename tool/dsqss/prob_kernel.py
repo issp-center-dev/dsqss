1126242739c9eb326ff4c34b2c270770a4715e79
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2021-03-07 17:31:13 +0900

    update tools installation

--- tool/dsqss/prob_kernel.py
+++ tool/dsqss/prob_kernel.py
@@ -14,7 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Sequence, Protocol
+from typing import Sequence
+from typing_extensions import Protocol
 
 import numpy as np
 
